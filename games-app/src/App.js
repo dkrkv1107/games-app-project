2c8cc33da108d795ef4f56e839c5f0aa72ca4f05
dkrkv1107 <dkrkv1107@icloud.com>
2022-07-14 13:17:06 +0530

    added search feature

--- games-app/src/App.js
+++ games-app/src/App.js
@@ -12,26 +12,49 @@ function App() {
   }
   const [search, setSearch] = useState('');
   const [games, setGames] = useState([]);
+  const [order, setOrder] = useState('asc');
 
   useEffect(() => {
     getData();
 
   }, [])
+  const Inc = () => {
+    games.score.sort((a, b) =>
+      a.score - b.score``
+    );
+  }
   return (
     <div className="App">
       <div className='main'>
         <header className='header'>Games Store</header>
         <form className='main'>
           <div className='main-search'>
-            <input type='text' placeholder='Search...' onChange={(e) => setSearch(e.target.value)} />
-            <input type='submit' value='Search' />
+            <input type='text' placeholder='Search by Name or Platform' onChange={(e) => setSearch(e.target.value)} />
+
+
+            <div className="dropdown">
+              <button className="dropbtn">Sort</button>
+              <div className="dropdown-content">
+                <a href="#" onClick={Inc}>Asc</a>
+                <a href="#">Desc</a>
+
+              </div>
+            </div>
           </div>
 
 
         </form>
         <div className='main-content'>
-          {games.map((gamer, index) => {
-            return <div>
+          {games.slice(1).filter((val) => {
+            if (search === '') {
+              return val;
+            } else if (val.title.toLowerCase().includes(search.toLowerCase())) {
+              return val;
+            } else if (val.platform.toLowerCase().includes(search.toLowerCase())) {
+              return val;
+            }
+          }).map((gamer, index) => {
+            return <div key={index}>
               <h3>Title: {gamer.title}</h3>
               <h3>Platform: {gamer.platform}</h3>
               <h3>Score: {gamer.score}</h3>
