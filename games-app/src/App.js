64cba8dba772f64c53e804ce0c19b268495ce10f
dkrkv1107 <dkrkv1107@icloud.com>
2022-07-14 16:38:19 +0530

    new commits

--- games-app/src/App.js
+++ games-app/src/App.js
@@ -1,6 +1,7 @@
-import Axios from 'axios';
-import './App.css';
-import React, { useState, useEffect } from 'react';
+import Axios from "axios";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import GamerCard from "./GamerCard";
 
 
 function App() {
@@ -8,60 +9,72 @@ function App() {
   async function getData() {
     var result = await Axios.get(url);
     setGames(result.data);
-
   }
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [games, setGames] = useState([]);
-  const [order, setOrder] = useState('asc');
 
   useEffect(() => {
     getData();
+  }, []);
 
-  }, [])
-  const Inc = () => {
-    games.score.sort((a, b) =>
-      a.score - b.score``
-    );
+
+  function asc() {
+
+    setGames(games.sort((a, b) => {
+      return a.score - b.score;
+    })
+
+    )
+
+    console.log(games);
   }
-  return (
-    <div className="App">
-      <div className='main'>
-        <header className='header'>Games Store</header>
-        <form className='main'>
-          <div className='main-search'>
-            <input type='text' placeholder='Search by Name or Platform' onChange={(e) => setSearch(e.target.value)} />
 
+
+  return (
+    // <img src={backimg} className="backimg" alt="backimg" />
+    <div className="App">
+      <div className="main">
+        <header className="header">Games Store</header>
+        <form className="main">
+          <div className="main-search">
+            <input
+              type="text"
+              placeholder="Search by Name or Platform"
+              onChange={(e) => setSearch(e.target.value)}
+            />
 
             <div className="dropdown">
-              <button className="dropbtn">Sort</button>
+              <button className="dropbtn">Sort by Score</button>
               <div className="dropdown-content">
-                <a href="#" onClick={Inc}>Asc</a>
+                <a onClick={asc} href="#">Asc</a>
                 <a href="#">Desc</a>
-
               </div>
             </div>
           </div>
-
-
         </form>
-        <div className='main-content'>
-          {games.slice(1).filter((val) => {
-            if (search === '') {
-              return val;
-            } else if (val.title.toLowerCase().includes(search.toLowerCase())) {
-              return val;
-            } else if (val.platform.toLowerCase().includes(search.toLowerCase())) {
-              return val;
-            }
-          }).map((gamer, index) => {
-            return <div key={index}>
-              <h3>Title: {gamer.title}</h3>
-              <h3>Platform: {gamer.platform}</h3>
-              <h3>Score: {gamer.score}</h3>
-              <h3>Genre: {gamer.genre}</h3>
-              <h3>Editor's Choice: {gamer.editors_choice}</h3>
-            </div>
-          })}
+        <div className="main-content">
+          {games
+            .slice(1)
+            .filter((val) => {
+              if (search === "") {
+                return val;
+              } else if (
+                val.title.toLowerCase().includes(search.toLowerCase())
+              ) {
+                return val;
+              } else if (
+                val.platform.toLowerCase().includes(search.toLowerCase())
+              ) {
+                return val;
+              }
+            })
+            .map((gamer, index) => {
+              return (
+                <div className="card" key={index}>
+                  {GamerCard(gamer)}
+                </div>
+              );
+            })}
         </div>
       </div>
     </div>
